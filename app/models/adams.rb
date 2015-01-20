78fdf2e3dc07970ac3d82686d09c3718242fd535
vervald <ifaper@gmail.com>
2015-01-20 22:23:21 +0400

    some fix + add id=5

--- app/models/adams.rb
+++ app/models/adams.rb
@@ -20,16 +20,19 @@ class Adams
     while x[-1] - h >= a
       if id == 1 then
         yt = y[-1] - h * f(x[-1], y[-1], func)
-        y << y[-1] - h * f(x[-1] + h, yt, func)
+        y << y[-1] - h * f(x[-1] - h, yt, func)
       elsif id == 2 then
         yt = y[-1] - h * (3 * f(x[-1], y[-1], func) - f(x[-2], y[-2], func)) / 2
-        y << y[-1] - h * (f(x[-1] + h, yt, func) + f(x[-1], y[-2], func)) / 2
+        y << y[-1] - h * (f(x[-1] - h, yt, func) + f(x[-1], y[-2], func)) / 2
       elsif id == 3 then
         yt = y[-1] - h * (23 * f(x[-1], y[-1], func) - 16 * f(x[-2], y[-2], func) + 5 * f(x[-3], y[-3], func)) / 12
-        y << y[-1] - h * (5 * f(x[-1] + h, yt, func) + 8 * f(x[-1], y[-1], func) - f(x[-2], y[-2], func)) / 12
+        y << y[-1] - h * (5 * f(x[-1] - h, yt, func) + 8 * f(x[-1], y[-1], func) - f(x[-2], y[-2], func)) / 12
       elsif id == 4 then
         yt = y[-1] - h * (55 * f(x[-1], y[-1], func) - 59 * f(x[-2], y[-2], func) + 37 * f(x[-3], y[-3], func) - 9 * f(x[-4], y[-4], func)) / 24
-        y << y[-1] - h * (9 * f(x[-1] + h, yt, func) + 19 * f(x[-1], y[-1], func) - 5 * f(x[-2], y[-2], func) + f(x[-3], y[-3], func)) / 24
+        y << y[-1] - h * (9 * f(x[-1] - h, yt, func) + 19 * f(x[-1], y[-1], func) - 5 * f(x[-2], y[-2], func) + f(x[-3], y[-3], func)) / 24
+      elsif id == 5 then
+        yt = y[-1] - h * (1901 * f(x[-1], y[-1], func) - 2774 * f(x[-2], y[-2], func) + 2616 * f(x[-3], y[-3], func) - 1274 * f(x[-4], y[-4], func) + 251 * f(x[-5], y[-5], func)) / 720
+        y << y[-1] - h * (251 * f(x[-1] - h, yt, func) + 646 * f(x[-1], y[-1], func) - 264 * f(x[-2], y[-2], func) + 106 * f(x[-3], y[-3], func) + 19 * f(x[-4], y[-4], func)) / 720
       end
       x << x[-1] - h
     end
@@ -58,6 +61,9 @@ class Adams
       elsif id == 4 then
         yt = y[-1] + h * (55 * f(x[-1], y[-1], func) - 59 * f(x[-2], y[-2], func) + 37 * f(x[-3], y[-3], func) - 9 * f(x[-4], y[-4], func)) / 24
         y << y[-1] + h * (9 * f(x[-1] + h, yt, func) + 19 * f(x[-1], y[-1], func) - 5 * f(x[-2], y[-2], func) + f(x[-3], y[-3], func)) / 24
+      elsif id == 5 then
+        yt = y[-1] + h * (1901 * f(x[-1], y[-1], func) - 2774 * f(x[-2], y[-2], func) + 2616 * f(x[-3], y[-3], func) - 1274 * f(x[-4], y[-4], func) + 251 * f(x[-5], y[-5], func)) / 720
+        y << y[-1] + h * (251 * f(x[-1] + h, yt, func) + 646 * f(x[-1], y[-1], func) - 264 * f(x[-2], y[-2], func) + 106 * f(x[-3], y[-3], func) + 19 * f(x[-4], y[-4], func)) / 720
       end
       x << x[-1] + h
     end
